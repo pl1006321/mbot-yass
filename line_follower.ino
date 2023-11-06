c2a6c620ba89d644acdef9aae7d420dfea5e1455
pl1006321 <149692472+pl1006321@users.noreply.github.com>
2023-11-06 10:43:05 -0500

    Update line_follower.ino

--- line_follower.ino
+++ line_follower.ino
@@ -2,6 +2,8 @@
 #include <MeMCore.h> 
 
 MeLineFollower linefinder(PORT_2);
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2); 
 
 void setup()
 {
@@ -13,7 +15,7 @@ void loop()
   // black = blue light off = 0
   // white = blue light on = 1
 
-  if (linefinder.readSensor1()==0 && linefinder.readSebsir2()==0)
+  if (linefinder.readSensor1()==0 && linefinder.readSensor2()==0)
   {
     Serial.println("Sensor 1 on black and Sensor 2 on black"); 
   }
@@ -23,7 +25,7 @@ void loop()
     Serial.println("Sensor 1 on white and Sensor 2 on black"); 
   }
 
-  else if (linefinder.readSensor1()==0 && linefinder.readSensor2()=1)
+  else if (linefinder.readSensor1()==0 && linefinder.readSensor2()==1)
   {
     Serial.println("Sensor 1 on black and Sensor 2 on white"); 
   }
@@ -32,4 +34,16 @@ void loop()
   {
     Serial.println("Sensor 1 on white and Sensor 2 on white"); 
   }
+
+  if (linefinder.readSensor1())
+  {
+    motor1.run(-100); 
+  }
+  else motor1.run(100); 
+
+  if (linefinder.readSensor2())
+  {
+    motor2.run(100);
+  }
+  else motor2.run(-100); 
 }
