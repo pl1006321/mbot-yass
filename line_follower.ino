02bb8e61b2bb0b62c3336dd2f27d49df2c3261ba
pl1006321 <149692472+pl1006321@users.noreply.github.com>
2023-11-07 10:45:32 -0500

    Update line_follower.ino

--- line_follower.ino
+++ line_follower.ino
@@ -5,6 +5,57 @@ MeLineFollower linefinder(PORT_2);
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2); 
 
+int value;
+
+
+
+void moveforward(int d) 
+{ 
+  motor1.run(-100);
+  motor2.run(100); 
+  delay(d); 
+  motor1.stop();
+  motor2.stop();  
+}
+
+void movebackward(int d)
+{
+  motor1.run(100);
+  motor2.run(-100);
+  delay(d); 
+  motor1.stop();
+  motor2.stop();
+}
+
+void spin(int d) // d = 1000 is optimal depending on surface
+{
+  motor1.run(100);
+  motor2.run(115); 
+  delay(); 
+  motor1.stop();
+  motor2.stop();
+}
+
+void left(int d)
+{
+  motor1.run(100);
+  motor2.run(100);  
+  delay(100); 
+  motor1.stop();
+  motor2.stop(); 
+  moveforward(100); 
+}
+
+void right(int d) // d = 1250 was optimal for this 
+{
+  motor1.run(-100); 
+  motor2.run(-100);
+  delay(100);
+  motor1.stop(); 
+  motor2.stop(); 
+  moveforward(100); 
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -18,32 +69,46 @@ void loop()
   if (linefinder.readSensor1()==0 && linefinder.readSensor2()==0)
   {
     Serial.println("Sensor 1 on black and Sensor 2 on black"); 
+    value = 1; 
   }
 
   else if (linefinder.readSensor1()==1 && linefinder.readSensor2()==0)
   {
     Serial.println("Sensor 1 on white and Sensor 2 on black"); 
+    value = 2;
   }
 
   else if (linefinder.readSensor1()==0 && linefinder.readSensor2()==1)
   {
     Serial.println("Sensor 1 on black and Sensor 2 on white"); 
+    value = 3; 
   }
 
   else // (linefinder.readSensor1()==1 && linefinder.readSensor2()==1)
   {
     Serial.println("Sensor 1 on white and Sensor 2 on white"); 
+    value = 4;
   }
 
-  if (linefinder.readSensor1())
+  if (value == 1)
   {
-    motor1.run(-100); 
+    moveforward(100); 
   }
-  else motor1.run(100); 
 
-  if (linefinder.readSensor2())
+  else if (value == 2)
   {
-    motor2.run(100);
+    right(100); 
   }
-  else motor2.run(-100); 
+
+  else if (value == 3)
+  {
+    left(100); 
+  }
+
+  else // (if value == 4)
+  {
+    while (value == 4);
+    
+  }
+
 }
